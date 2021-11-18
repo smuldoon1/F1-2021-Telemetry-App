40b636e133599b98325de3bcc8120d157e5bca9d
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2021-11-18 20:52:42 +0000

    Added speed trace graph
    A graph showing the speed to lap distance of each driver is shown on the window.
    
    Each speed trace is coloured using their teams colour code (Current F1 2021 teams only, other teams may result in error). Drivers in the same team are differentiated with a different thickness of trace line.

--- appTest.py
+++ appTest.py
@@ -8,8 +8,15 @@ driverName = StringVar()
 lastLapTime = StringVar()
 currentLapTime = StringVar()
 
-positionX = 0
-positionZ = 0
+newSpeed = [0] * 22
+speed = [0] * 22
+newDistance = [0] * 22
+distance = [0] * 22
+trackDistance = 0
+
+showDriverTrace = [False] * 22
+driverColours = ["#000"] * 22
+driverWidths = [1] * 22
 
 def msToString(ms):
     milliseconds = (ms % 1000)
@@ -24,32 +31,72 @@ def getValidityTextColour(boolean):
         return "#000"
     return "#f00"
 
+def getTeamColour(teamId):
+    if teamId == 0: # Mercedes
+        return "#00D2BE"
+    if teamId == 1: # Ferrari
+        return "#DC0000"
+    if teamId == 2: # Red bull
+        return "#0600EF"
+    if teamId == 3: # Williams
+        return "#005AFF"
+    if teamId == 4: # Aston martin
+        return "#006F62"
+    if teamId == 5: # Alpine
+        return "#0090FF"
+    if teamId == 6: # Alpha tauri
+        return "#2B4562"
+    if teamId == 7: # Haas
+        return "#FFFFFF"
+    if teamId == 8: # Mclaren
+        return "#FF8700"
+    if teamId == 9: # Alfa
+        return "#900000"
+
 def task():
-    #canvas.delete("all")
+    global distance, newSpeed, speed, trackDistance
     packet = RetrievePacket()
-    if (packet.packetHeader.packetID == 0):
-        positionX = 200 + packet.carMotionData[packet.packetHeader.playerCarIndex].worldPositionX / 5
-        positionZ = 200 + packet.carMotionData[packet.packetHeader.playerCarIndex].worldPositionZ / 5
-        canvas.create_oval(positionX, positionZ, positionX + 10, positionZ + 10, fill = "white", outline = "white")
+    if (packet.packetHeader.packetID == 1):
+        trackDistance = packet.trackLength
     if (packet.packetHeader.packetID == 2):
         lastLapTime.set("Previous Lap Time: " + msToString(packet.lapData[packet.packetHeader.playerCarIndex].lastLapTime))
         currentLapTime.set("Current Lap Time: " + msToString(packet.lapData[packet.packetHeader.playerCarIndex].currentLapTime))
         currentLapLabel.config(fg=getValidityTextColour(packet.lapData[packet.packetHeader.playerCarIndex].currentLapInvalid))
+        for i in range(22):
+            newDistance[i] = packet.lapData[i].lapDistance
+            if (showDriverTrace[i] == True and trackDistance != 0 and newDistance[i] > 0 and newDistance[i] < trackDistance and newDistance > distance):
+                speedTrace.create_line(distance[i] / (trackDistance / 1000), 400 - speed[i], newDistance[i] / (trackDistance / 1000), 400 - newSpeed[i], fill = driverColours[i], width = driverWidths[i])
+            speed[i] = newSpeed[i]
+            distance[i] = newDistance[i]
     if (packet.packetHeader.packetID == 4):
         driverName.set("Driver: " + packet.participants[packet.packetHeader.playerCarIndex].name)
-        a = 1
+        teamIdOccurences = []
+        for i in range(22):
+            driver = packet.participants[i].driverID
+            if driver > -1:
+                showDriverTrace[i] = True
+            team = packet.participants[i].teamID
+            driverColours[i] = getTeamColour(team)
+            if team not in teamIdOccurences:
+                driverWidths[i] = 1
+                teamIdOccurences.append(team)
+            else:
+                driverWidths[i] = 3
+    if (packet.packetHeader.packetID == 6):
+        for i in range(22):
+            newSpeed[i] = packet.carTelemetryData[i].speed
     root.after(1, task)
 
 root.title("F1 2021 Telemetry App")
-root.geometry("400x500+100+100")
+root.geometry("1000x600+100+100")
 
 Label(root, textvariable=driverName).pack()
 Label(root, textvariable=lastLapTime).pack()
 currentLapLabel = Label(root, textvariable=currentLapTime)
 currentLapLabel.pack()
 
-canvas = Canvas(root, width=400, height=400, bg="black")
-canvas.pack()
+speedTrace = Canvas(root, width=1000, height=400, bg="black")
+speedTrace.pack()
 
 root.after(1, task)
 root.mainloop()
