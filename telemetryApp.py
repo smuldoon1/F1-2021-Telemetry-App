88a405bb5d597e460101f3a29d7a38da45fa30b4
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-03-03 22:30:58 +0000

    Added labels to module frame

--- telemetryApp.py
+++ telemetryApp.py
@@ -9,18 +9,21 @@ import math
 
 root = Tk()
 
+root.title("F1 2021 Telemetry App")
+root.geometry("{}x{}".format(1200, 800))
+root.configure(background="#212026")
+
+telemetry_modules = []
+
 def updateData():
     #packet = RetrievePacket()
 
-    timingBoard.updateSize()
-    paceGraph.updateSize()
-    pitMonitor.updateSize()
-    fuelMonitor.updateSize()
-    pitStrategy.updateSize()
+    for telemetry_module in telemetry_modules:
+        telemetry_module.frame.after(1, telemetry_module.updateSize)
 
-    root.geometry("{}x{}".format(round_to_multiple(root.winfo_width(), 5), round_to_multiple(root.winfo_height(), 3)))
+    root.geometry("{}x{}".format(round_to_multiple(root.winfo_width(), 12), round_to_multiple(root.winfo_height(), 8)))
 
-    root.after(1, task)
+    root.after(10, updateData)
 
 def task():
     thread = Thread(target = updateData)
@@ -29,14 +32,18 @@ def task():
 def round_to_multiple(x, base):
     return base * round(x / base)
 
-root.title("F1 2021 Telemetry App")
-root.geometry("{}x{}".format(1280, 720))
+def create_telemetry_module(name, column, row, x_span, y_span, colour):
+    new_module = TelemetryModule(root, name, column, row, x_span, y_span, colour)
+    telemetry_modules.append(new_module)
 
-timingBoard = TelemetryModule(root, 0, 0, 2, 2, "red")
-paceGraph = TelemetryModule(root, 2, 0, 3, 2, "blue")
-pitMonitor = TelemetryModule(root, 0, 2, 1, 1, "yellow")
-fuelMonitor = TelemetryModule(root, 1, 2, 1, 1, "green")
-pitStrategy = TelemetryModule(root, 2, 2, 3, 1, "brown")
+create_telemetry_module("Timing Tower", 0, 0, 3, 6, "gray")
+create_telemetry_module("Pace Tower", 3, 0, 2, 6, "blue")
+create_telemetry_module("Pace Graph", 5, 0, 7, 3, "red")
+create_telemetry_module("Predicted Finish Graph", 5, 3, 7, 3, "green")
+create_telemetry_module("Pit Monitor", 0, 6, 2, 2, "yellow")
+create_telemetry_module("Fuel Monitor", 2, 6, 2, 2, "orange")
+create_telemetry_module("Weather Forecast", 4, 6, 8, 1, "cyan")
+create_telemetry_module("Pit Strategy", 4, 7, 8, 1, "pink")
 
-root.after(1, task)
+root.after(1, updateData)
 root.mainloop()
--- telemetryModule.py
+++ telemetryModule.py
@@ -1,17 +1,17 @@
 from tkinter import *
 
 class TelemetryModule():
-
-    frame = None
-
-    def __init__(self, root, column, row, x_span, y_span, colour):
-        self.frame = Frame(root, bg = colour)
+    def __init__(self, root, name, column, row, x_span, y_span, colour):
         self.root = root
+        self.name = name
         self.x_span = x_span
         self.y_span = y_span
 
-        self.frame.grid(row = row, column = column, columnspan = x_span, rowspan = y_span)
+        self.frame = Frame(root, bg = colour)
+        self.header = Label(self.frame, text = name)
+        self.frame.grid(row = row, column = column, columnspan = x_span, rowspan = y_span, padx = 5, pady = 5)
+        self.header.grid()
 
     def updateSize(self):
-        self.frame.config(width = int(self.root.winfo_width() / 5) * self.x_span)
-        self.frame.config(height = int(self.root.winfo_height() / 3) * self.y_span)
+        self.frame.config(width = int(self.root.winfo_width() / 12) * self.x_span - 10)
+        self.frame.config(height = int(self.root.winfo_height() / 8) * self.y_span - 10)
